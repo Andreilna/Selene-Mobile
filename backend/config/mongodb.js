81074ff4f4422fd30eac9c4f1a1635c56427e459
Andreilna <andreialda7@gmail.com>
2026-04-22 13:52:34 -0300

    Fix: Atualização do mongo

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/selene';
-
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGODB_URI);
+    if (!process.env.MONGODB_URI) {
+      throw new Error('MONGODB_URI não definida no .env');
+    }
+
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`✅ MongoDB Conectado: ${conn.connection.host}`);
   } catch (error) {
