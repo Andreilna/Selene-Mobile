6a80c7919380d257cc45b3b11462d78fd2cd822e
Andreilna <andreialda7@gmail.com>
2026-05-02 18:05:01 -0300

    fix: rota de dashboard admin

--- backend/routes/adminDashboardRoutes.js
+++ backend/routes/adminDashboardRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const adminAuthMiddleware = require("../middleware/adminAuthMiddleware");
-const AdminDashboardController = require("../controllers-mongodb/adminDashboardController");
+const adminAuthMiddleware = require("../middleware/admin-auth-mongodb");
+const AdminDashboardController = require("../controllers-mongodb/AdminDashboardController");
 
 // 📊 STATS GERAIS DO SISTEMA (ADMIN)
 router.get("/stats", adminAuthMiddleware, AdminDashboardController.stats);
